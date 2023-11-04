429f89aa0ce8074e0ccb2ae03b1642202197b9b4
TooBunReal <khocvole1kg3@gmail.com>
2023-11-05 04:01:31 +0700

    success connect api

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-// import SideBar from './Components/SideBar/SideBar';
-// import HomePage from './Components/HomePage/Body/HomePage';
-// import UserProfile from './Components/UserProfile/UserProfile';
-// import LogIn from './Components/LogInPage/Login';
-// import GamePage from './Components/GamePage/GamePage';
-// import Register from './Components/RegisterPage/Register';
 import './App.css';
 import axios from 'axios';
-
-/*Routing*/
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 class App extends Component {
   state = {
     message: ''
-  };
+  }
 
   componentDidMount() {
     axios.get('/api/test')
       .then(result => this.setState({ message: result.data.message }))
-  };
+  }
 
   render() {
     return (
@@ -32,7 +23,7 @@ class App extends Component {
         </header>
       </div>
     )
-  };
-};
+  }
+}
 
 export default App;
