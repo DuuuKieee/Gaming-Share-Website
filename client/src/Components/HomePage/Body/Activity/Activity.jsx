3ad996025350f38f4e4bbb045c65e102edf9dc29
KaiMink <huynhdinhkhaiminh@gmail.com>
2023-11-15 17:51:24 +0700

    Homepage

--- client/src/Components/HomePage/Body/Activity/Activity.jsx
+++ client/src/Components/HomePage/Body/Activity/Activity.jsx
@@ -1,58 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Activity.scss";
 import img1 from "../../../../Assets/Video Projects/a52.jpg";
+import GameList from "./GameList/GameList";
 
-const Activity = () => {
+//fetch data trên mongodb về xong tạo thành trường game 
+const ListGame = () => {
+    const [gamesData, setGamesData] = useState([]);
+    //test
+    const name = "Game-X";
+    const description = "aaaa";
     return (
         <div className="Body flex">
             <ul className="GameBoxes grid">
-                <li className="GameBox flex">
-                    <a href="Game-X" target="_self">
-                    <img src={img1} className="GameIcon"></img>
-                    <div className="GameContent grid">
-                        <p>Game 1</p>
-                        <p>Description/Like</p>
-                    </div>
-                    </a>
-                </li>
-                <li className="GameBox flex">
-                    <img src={img1} className="GameIcon"></img>
-                    <div className="GameContent grid">  
-                        <p>Game 2</p>
-                        <p>Description/Like</p>
-                    </div>
-                </li>
-                <li className="GameBox flex">
-                    <img src={img1} className="GameIcon"></img>
-                    <div className="GameContent grid">
-                        <p>Game 3</p>
-                        <p>Description/Like</p>
-                    </div>
-                </li>
-                <li className="GameBox flex">
-                    <img src={img1} className="GameIcon"></img>
-                    <div className="GameContent grid">
-                        <p>Game 4</p>
-                        <p>Description/Like</p>
-                    </div>
-                </li>
-                <li className="GameBox flex">
-                    <img src={img1} className="GameIcon"></img>
-                    <div className="GameContent grid">
-                        <p>Game 5</p>
-                        <p>Description/Like</p>
-                    </div>
-                </li>
-                <li className="GameBox flex">
-                    <img src={img1} className="GameIcon"></img>
-                    <div className="GameContent grid">
-                        <p>Game 6</p>
-                        <p>Description/Like</p>
-                    </div>
-                </li>
+                {gamesData.map((game) => (
+                    <GameList key={game.id} game={game} />
+                ))}
+                <GameList imgSrc={img1} gameName={name} description={description} />
             </ul>
         </div>
     )
 }
 
-export default Activity
+export default ListGame
---
+++ client/src/Components/HomePage/Body/Activity/GameList/GameList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./GameList.scss";
+//import icon
+import { AiOutlineLike } from "react-icons/ai";
+import { BiDislike } from "react-icons/bi";
+const GameList =({ imgSrc, gameName, description } ) =>{
+    return (
+        <div className="GameBox flex">
+            <a href="Game-X" target="_self">
+                <img src={imgSrc} className="GameIcon"></img>
+                    <div className="GameContent grid">
+                        <p>{gameName}</p>
+                        <p>{description}</p>
+                </div>
+            </a>
+            <div className="emo grid">
+                <button className="like"><AiOutlineLike /></button>
+                <button className="dislike"><BiDislike /></button>
+            </div>
+        </div>
+      );
+}
+export default GameList;
