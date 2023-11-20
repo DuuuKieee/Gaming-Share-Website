7f0ced3c21bccbc331ecdbc08cce6d14d0f35f67
DuuuKieee 1604 <tranthanhloisd@gmail.com>
2023-11-21 02:18:04 +0700

    PlayGamePage

--- client/src/Components/GamePage/GamePage.jsx
+++ client/src/Components/GamePage/GamePage.jsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./GamePage.scss";
-import { Unity, useUnityContext } from "react-unity-webgl";
 
 const GamePage = () => {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "http://localhost:8000/Build/WebDevelopmentProject-main.loader.js",
-    dataUrl: "http://localhost:8000/Build/WebDevelopmentProject-main.data.unityweb",
-    frameworkUrl: "http://localhost:8000/Build/WebDevelopmentProject-main.framework.js.unityweb",
-    codeUrl: "http://localhost:8000/Build/WebDevelopmentProject-main.wasm.unityweb",
-  });
+  const [responseData, setResponseData] = useState();
+
+  const playGame = () => {
+    fetch("http://localhost:8000/api/playgame", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        gamename: "Legend of Sontinh",
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Xử lý thành công
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        // Xử lý dữ liệu trả về
+        setResponseData(data.gameData); // Lưu giá trị data vào state
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="gamePageContent">
       <div className="bodyGame">
         <p>Game X</p>
-        <div >
-          <Unity className="GameWindow" unityProvider={unityProvider} />
+        <div>
+          <button onClick={playGame}>Play</button>
+          {responseData&&(
+            <iframe
+              src={`http://localhost:8000/games/unzip/${responseData}/index.html`}
+              title="Your Page"
+              width="100%"
+              height="500px"
+            />
+          )}
         </div>
       </div>
     </div>
