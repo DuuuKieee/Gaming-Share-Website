61747755621b9949534a8ddbcd5c400a55a718a9
t0obUnR34L <khocvole1kg3@gmail.com>
2023-11-26 20:27:22 +0700

    'fix_SideBar.jsx'

--- client/src/Components/SideBar/SideBar.jsx
+++ client/src/Components/SideBar/SideBar.jsx
@@ -1,41 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./SideBar.scss";
-
+import Cookies from "js-cookie";
 import logo from "../../Assets/logo.png"
 const SideBar = () => {
     const [isLogin, setIsLogin] = useState(true);
-    return (
-        <div className="sideBar">
-            <div className="logo flex">
-                <a href="/" className="noneColorLink">
-                    <img src={logo} alt="Img Name"></img>
-                    <h2 style={{ fontSize: "var(--h3Fontsize)",color: "var(--titleText)"  }}>GameWeb</h2>
-                </a>
+    const token = Cookies.get("token");
+    if (token) {
+        return (
+            <div className="sideBar">
+                <div className="logo flex">
+                    <a href="/" className="noneColorLink">
+                        <img src={logo} alt="Img Name"></img>
+                        <h2 style={{ fontSize: "var(--h3Fontsize)", color: "var(--titleText)" }}>GameWeb</h2>
+                    </a>
+                </div>
+                <div className="Tag">
+                    <h3 className="Title">
+                        MENU
+                    </h3>
+                    <ul className="UserMenuList grid">
+                        <li className="ListItems">
+                            <a href={isLogin ? "/User-Profile" : "/log-in"} className="tagLink">
+                                Trang cá nhân
+                            </a>
+                        </li>
+                        {/* <li className="ListItems">
+                            <a href="/log-in" className="tagLink">
+                                Đăng nhập
+                            </a>
+                        </li>
+                        <li className="ListItems">
+                            <a href="/Register" className="tagLink">
+                                Đăng ký
+                            </a>
+                        </li> */}
+                    </ul>
+                </div>
             </div>
-            <div className="Tag">
-                <h3 className="Title">
-                    MENU
-                </h3>
-                <ul className="UserMenuList grid">
-                    <li className="ListItems">
-                        <a href={isLogin ? "/User-Profile" : "/log-in"} className="tagLink">
-                            Trang cá nhân
-                        </a>
-                    </li>
-                    <li className="ListItems">
-                        <a href="/log-in" className="tagLink">
-                            Đăng nhập
-                        </a>
-                    </li>
-                    <li className="ListItems">
-                        <a href="/Register" className="tagLink">
-                            Đăng ký
-                        </a>
-                    </li>
-                </ul>
+        )
+    }
+    else {
+        return (
+            <div className="sideBar">
+                <div className="logo flex">
+                    <a href="/" className="noneColorLink">
+                        <img src={logo} alt="Img Name"></img>
+                        <h2 style={{ fontSize: "var(--h3Fontsize)", color: "var(--titleText)" }}>GameWeb</h2>
+                    </a>
+                </div>
+                <div className="Tag">
+                    <h3 className="Title">
+                        MENU
+                    </h3>
+                    <ul className="UserMenuList grid">
+                        {/* <li className="ListItems">
+                            <a href={isLogin ? "/User-Profile" : "/log-in"} className="tagLink">
+                                Trang cá nhân
+                            </a>
+                        </li> */}
+                        <li className="ListItems">
+                            <a href="/log-in" className="tagLink">
+                                Đăng nhập
+                            </a>
+                        </li>
+                        <li className="ListItems">
+                            <a href="/Register" className="tagLink">
+                                Đăng ký
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 export default SideBar
